9f23d8fb0dafd64e47d8d9a3a8eaa2f6154fcfd0
thezmmm <1563081082@qq.com>
2026-01-14 19:01:47 +0200

    fix: fix eslint error

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-    testDir: 'e2e-tests',
-    timeout: 30000,
-    use: {
-        headless: true,
-        baseURL: 'http://localhost:8080',
-    },
+  testDir: 'e2e-tests',
+  timeout: 30000,
+  use: {
+    headless: true,
+    baseURL: 'http://localhost:8080',
+  },
 })
